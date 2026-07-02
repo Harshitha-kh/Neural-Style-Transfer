c88a8c2ee6093f9c1dfa57a89e16942983615e39
Harshitha-kh <harshithakh1504@gmail.com>
2026-07-02 22:55:47 +0530

    Initial commit

--- NST_Code/app.py
+++ NST_Code/app.py
@@ -35,7 +35,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 encoder = VGGEncoder('vgg_normalised.pth').to(device)
 decoder = Decoder().to(device)
-decoder.load_state_dict(torch.load('/home/ubuntu/Desktop/NST_Code/experiment/final_exp/decoder_final.pth'))
+decoder.load_state_dict(
+    torch.load(
+        r'C:\Users\harsh\OneDrive\Desktop\NST_project\ai-nst-project\NST_Code\experiment\final_exp\decoder_final.pth',
+        map_location=device
+    )
+)
 
 encoder.eval()
 decoder.eval()
